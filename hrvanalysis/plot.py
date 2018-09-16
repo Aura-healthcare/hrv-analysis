3e61d4a61a946aca2fe3215b494da71003e40439
robinchampseix <robin.champseix@gmail.com>
2018-09-16 10:24:44 +0200

    modif plot function

--- hrvanalysis/plot.py
+++ hrvanalysis/plot.py
@@ -36,7 +36,7 @@ def plot_distrib(nn_intervals, bin_length=8):
 
     style.use('ggplot')
     plt.figure(figsize=(12, 8))
-    plt.title("Density of Rr Intervals")
+    plt.title("Distribution of Rr Intervals", fontsize=20)
     plt.xlabel("Time (s)", fontsize=15)
     plt.ylabel("Number of Rr Interval per bin", fontsize=15)
     plt.hist(nn_intervals, bins=range(min_nn_i - 10, max_nn_i + 10, bin_length))
@@ -55,7 +55,6 @@ def plot_psd(nn_intervals, method="Welch", sampling_frequency=7, interpolation_m
     set to 7 Hz. No need to specify if Lomb method is used.
     interpolation_method - kind of interpolation as a string, by default "linear". No need to specify if lomb method
     is used.
-
     """
 
     freq, psd = get_freq_psd_from_nn_intervals(nn_intervals=nn_intervals, method=method,
@@ -70,7 +69,7 @@ def plot_psd(nn_intervals, method="Welch", sampling_frequency=7, interpolation_m
         plt.title("Lomb's periodogram", fontsize=20)
         plt.plot(freq, psd / (1000 * len(psd)))
     elif method == "Welch":
-        plt.title("Welch's periodogram", fontsize=20)
+        plt.title("FFT Spectrum : Welch's periodogram", fontsize=20)
         plt.plot(freq, psd / (1000 * len(psd)))
     else:
         raise ValueError("Not a valid method. Choose between 'Lomb' and 'Welch'")
@@ -97,7 +96,7 @@ def plot_poincare(nn_intervals):
 
     style.use('ggplot')
     plt.figure(figsize=(12, 8))
-    plt.title("Pointcare / Lorentz Plot", )
+    plt.title("Pointcare / Lorentz Plot", fontsize=20)
     plt.xlabel('NN_n (s)', fontsize=15)
     plt.ylabel('NN_n+1 (s)', fontsize=15)
     plt.scatter(ax1, ax2, c='r', s=12)
